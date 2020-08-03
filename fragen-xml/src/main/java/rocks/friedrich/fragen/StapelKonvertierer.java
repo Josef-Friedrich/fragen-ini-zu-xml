599ee4f3a72a9fece0d017cd2c222e6689e4dcbf
Josef Friedrich <josef@friedrich.rocks>
2020-08-03 08:15:30 +0200

    Staplekonvertierer

--- fragen-xml/src/main/java/rocks/friedrich/fragen/StapelKonvertierer.java
+++ fragen-xml/src/main/java/rocks/friedrich/fragen/StapelKonvertierer.java
@@ -5,6 +5,29 @@ import java.nio.file.*;
 import java.nio.file.attribute.*;
 
 public class StapelKonvertierer {
+  Path quelle;
+
+  public StapelKonvertierer (Path quelle) {
+    this.quelle = quelle;
+  }
+
+  public void konvertiere () throws IOException {
+    Files.walkFileTree(quelle, new SimpleFileVisitor<Path>() {
+      public FileVisitResult visitFile(Path quelle, BasicFileAttributes attrs) throws IOException {
+        Path ziel = Paths.get(quelle.toString().replaceAll("ini", "xml").replaceAll("\\.txt", ".xml"));
+        konvertiereEineDatei(quelle, ziel);
+        return FileVisitResult.CONTINUE;
+      }
+    });
+  }
+
+  private void konvertiereEineDatei (Path quelle, Path ziel) throws IOException {
+    if (gibDateiErweiterung(quelle.toFile()).equals("txt")) {
+      System.out.println(quelle);
+      System.out.println(ziel);
+      Files.createDirectories(ziel.getParent());
+    }
+  }
 
   private static String gibDateiErweiterung(File file) {
     String name = file.getName();
@@ -17,21 +40,10 @@ public class StapelKonvertierer {
 
   public static void main(String... args) throws IOException {
     ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-    String wurzelname = classloader.getResource(".").getFile();
-    System.out.println(wurzelname);
-    Path path = Paths.get(wurzelname);
-    System.out.println(path.toAbsolutePath());
+    String quelle = classloader.getResource(".").getFile();
 
-    Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-      public FileVisitResult visitFile(Path quelle, BasicFileAttributes attrs) throws IOException {
-        if (gibDateiErweiterung(quelle.toFile()).equals("txt")) {
-          System.out.println(quelle);
-          Path ziel = Paths.get(quelle.toString().replaceAll("ini", "xml"))
-          System.out.println(ziel);
-          Files.createDirectories(ziel.getParent());
-        }
-        return FileVisitResult.CONTINUE;
-      }
-    });
+    StapelKonvertierer konvertierer = new StapelKonvertierer(Paths.get(quelle));
+    konvertierer.konvertiere();
+
   }
 }
